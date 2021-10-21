05cc9f9621a851d40b53f31306cd43de81cba7bc
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2021-10-21 21:58:13 +0800

    Update __init__.py
    remove unused file

--- pyemits/__init__.py
+++ pyemits/__init__.py
@@ -6,6 +6,5 @@ so that you can use the api call like pd.DataFrame, pd.to_datetime
 from pyemits import common
 from pyemits import core
 from pyemits import evaluation
-from pyemits import forecast
 from pyemits import io
 from pyemits.common import utils
