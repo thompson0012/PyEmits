73a1813c2c41512353cb1722a3b4b71c3cc54b61
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2021-10-22 14:38:01 +0800

    Update nn.py

--- pyemits/core/ml/regression/nn.py
+++ pyemits/core/ml/regression/nn.py
@@ -1,30 +1,9 @@
 from keras import Sequential
-from keras.layers import Dense, Activation, Dropout, LSTM
+from keras.layers import Dense, Dropout, LSTM
+from pyemits.core.ml.base import WrapperBase, NeuralNetworkWrapperBase
 
 
-class WrapperBase:
-    def __init__(self, nn_model_obj, nickname=None):
-        self._nn_model_obj = nn_model_obj
-        self._nickname = nickname
-
-    @property
-    def nn_model_obj(self):
-        return self._nn_model_obj
-
-    def _fit(self, *args, **kwargs):
-        return self.nn_model_obj.fit(*args, **kwargs)
-
-    def fit(self, *args, **kwargs):
-        return self._fit(*args, **kwargs)
-
-    def _predict(self, *args, **kwargs):
-        return self.nn_model_obj.predict(*args, **kwargs)
-
-    def predict(self, *args, **kwargs):
-        return self._predict(*args, **kwargs)
-
-
-class KerasWrapper(WrapperBase):
+class KerasWrapper(NeuralNetworkWrapperBase):
     def __init__(self, keras_model_obj=Sequential(), nickname=None):
         super(KerasWrapper, self).__init__(keras_model_obj, nickname)
 
@@ -57,19 +36,31 @@ class KerasWrapper(WrapperBase):
         model.compile(loss='mse', optimizer='adam', metrics=['mse'])
         return cls(model)
 
+    def _fit(self, *args, **kwargs):
+        return self.model_obj.fit(*args, **kwargs)
+
+    def fit(self, *args, **kwargs):
+        return self._fit(*args, **kwargs)
+
+    def _predict(self, *args, **kwargs):
+        return self.model_obj.predict(*args, **kwargs)
+
+    def predict(self, *args, **kwargs):
+        return self._predict(*args, **kwargs)
+
     def __str__(self):
         return f"KerasWrapper_{self._nickname}"
 
 
 # TODO
-class PyTorchWrapper(WrapperBase):
+class PyTorchWrapper(NeuralNetworkWrapperBase):
     def __init__(self, pytorch_model_obj, nickname=None):
         super(PyTorchWrapper, self).__init__(pytorch_model_obj, nickname)
 
     def _fit(self, *args, **kwargs):
         import pytorch_lightning as pl
         trainer = pl.Trainer()
-        return trainer.fit(self.nn_model_obj, *args, **kwargs)
+        return trainer.fit(self.model_obj, *args, **kwargs)
 
     def fit(self, *args, **kwargs):
         return self._fit(*args, **kwargs)
@@ -79,6 +70,6 @@ class PyTorchWrapper(WrapperBase):
 
 
 # TODO
-class MXNetWrapper(WrapperBase):
+class MXNetWrapper(NeuralNetworkWrapperBase):
     def __init__(self, mxnet_model_obj, nickname=None):
         super(MXNetWrapper, self).__init__(mxnet_model_obj, nickname)
