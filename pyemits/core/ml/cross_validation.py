097b11a530ed0d94b0657ce491625ce878f936de
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2021-10-25 22:05:17 +0800

    Update cross_validation.py

--- pyemits/core/ml/cross_validation.py
+++ pyemits/core/ml/cross_validation.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.model_selection import KFold
-from pyemits.common.config_model import KFoldConfig, asdict_data_cls, ConfigBase
+from pyemits.common.config_model import KFoldConfig, asdict_data_cls, BaseConfig
 from pyemits.common.data_model import KFoldCVDataModel,RegressionDataModel
 from pyemits.common.py_native_dtype import SliceableDeque
 from typing import Tuple, Union
@@ -11,7 +11,7 @@ class KFoldCV:
     only split data into K-fold CV format, ml training will be handled by other module
     """
 
-    def __init__(self, data_model: Union[KFoldCVDataModel, RegressionDataModel], kfold_config: Union[KFoldConfig,ConfigBase] = KFoldConfig()):
+    def __init__(self, data_model: Union[KFoldCVDataModel, RegressionDataModel], kfold_config: Union[KFoldConfig, BaseConfig] = KFoldConfig()):
         self.data_model = data_model
         self.X, self.y = data_model.X_data, data_model.y_data
         self.kf_config = dict(kfold_config)
