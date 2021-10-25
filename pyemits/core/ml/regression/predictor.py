25824b486a8e9b5913240df62e30602e04063139
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2021-10-25 22:05:12 +0800

    Update predictor.py

--- pyemits/core/ml/regression/predictor.py
+++ pyemits/core/ml/regression/predictor.py
@@ -1,4 +1,4 @@
-from pyemits.core.ml.base import PredictorBase
+from pyemits.core.ml.base import BasePredictor
 from pyemits.common.data_model import RegressionDataModel
 from pyemits.common.py_native_dtype import SliceableDeque
 from typing import List
@@ -6,7 +6,7 @@ import numpy as np
 from typing import Literal
 
 
-class RegPredictor(PredictorBase):
+class RegPredictor(BasePredictor):
     """
     universal regression predictor
     specify the train method, so it can detect the data structure and use appropriate prediction method
@@ -20,7 +20,7 @@ class RegPredictor(PredictorBase):
         self.train_method = train_method
 
     def _get_clf_models_shape_dim(self):
-        shape = np.shape(self.clf_models)
+        shape = np.shape(self._clf_models)
         return len(shape)
 
     def _init_clf_models_predictions(self):
@@ -32,7 +32,7 @@ class RegPredictor(PredictorBase):
         self._init_clf_models_predictions()
 
         if self.train_method == 'ParallelRegTrainer':
-            for item in self.clf_models:
+            for item in self._clf_models:
                 algo = item[0][0]
                 clf_obj = item[0][1]
                 prediction_result = clf_obj.predict(X)
@@ -46,14 +46,14 @@ class RegPredictor(PredictorBase):
 
         # AST can't recognize syntax like: a == (1 or 2)
         elif self.train_method == 'RegTrainer' or self.train_method == 'MultiOutputRegTrainer':
-            for item in self.clf_models:
+            for item in self._clf_models:
                 algo = item[0]
                 clf_obj = item[1]
                 prediction_result = clf_obj.predict(X)
                 self.clf_models_predictions.append(prediction_result)
 
         elif self.train_method == 'KFoldCVTrainer':
-            for each_kfold_container in self.clf_models:
+            for each_kfold_container in self._clf_models:
                 n_kfold = each_kfold_container[0]
                 model_containers = each_kfold_container[1]
 
