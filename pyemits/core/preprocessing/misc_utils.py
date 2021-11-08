cda1dddfb41dc86f4ec58f995956b587b58e0429
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2021-11-09 00:01:20 +0800

    Update misc_utils.py
    add reduce_memory_usage

--- pyemits/core/preprocessing/misc_utils.py
+++ pyemits/core/preprocessing/misc_utils.py
@@ -103,3 +103,57 @@ def fix_async_freq(df: pd.DataFrame):
     time_delta = get_freq_time_interval(arr)
 
     return df_.reindex(pd.date_range(arr[0], arr[-1], freq=time_delta))
+
+
+def reduce_memory_usage(df, verbose=True):
+    """
+    References
+    ----------
+    https://mp.weixin.qq.com/s/RkiFbnGios5eDLiXftBGAw
+
+    Parameters
+    ----------
+    df
+    verbose
+
+    Returns
+    -------
+
+    """
+    numerics = ["int8", "int16", "int32", "int64", "float16", "float32", "float64"]
+    start_mem = df.memory_usage().sum() / 1024 ** 2
+    for col in df.columns:
+        col_type = df[col].dtypes
+        if col_type in numerics:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            if str(col_type)[:3] == "int":
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)
+            else:
+                if (
+                        c_min > np.finfo(np.float16).min
+                        and c_max < np.finfo(np.float16).max
+                ):
+                    df[col] = df[col].astype(np.float16)
+                elif (
+                        c_min > np.finfo(np.float32).min
+                        and c_max < np.finfo(np.float32).max
+                ):
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+    end_mem = df.memory_usage().sum() / 1024 ** 2
+    if verbose:
+        print(
+            "Mem. usage decreased to {:.2f} Mb ({:.1f}% reduction)".format(
+                end_mem, 100 * (start_mem - end_mem) / start_mem
+            )
+        )
+    return df
