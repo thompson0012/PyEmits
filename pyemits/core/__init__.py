8e72f695d735b5bbf1f4980233c5bee0637ad6c8
thompson0012 <51963680+thompson0012@users.noreply.github.com>
2021-11-29 23:10:48 +0800

    Update __init__.py

--- pyemits/core/__init__.py
+++ pyemits/core/__init__.py
@@ -1,4 +1,4 @@
 """
-core layer, the fundamental parts, provide general function and method
-usually called in services layer
+core layer, the core function module, provide the minimal function or features that data scientist may use
+can be customized in services layer design
 """
